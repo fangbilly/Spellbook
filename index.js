768d72e18bff27c90e0733c7dcb01b9107533a82
fangz2 <fangz2@rose-hulman.edu>
2018-06-05 13:51:04 -0400

    update to davey's setup

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-console.log('testing')
-const button = document.querySelector('button')
-
-const changeContents = function() {
-  const heading = document.querySelector('#heading_2')
-  heading.textContent = 'Bonus Credit'
-  button.textContent ='yes'
-}
-
-button.addEventListener('click', changeContents)
-
 const form = document.querySelector('form')
 
-const formChangeHeading = function(ev) {
-   
+const changeHeading = function(ev) {
+  ev.preventDefault()
 
-   // var text= "#"+ form.elements['heading1'].value
-    const headinga = document.querySelector("#spells")
-    headinga.innerHTML += `<li>${form.elements['newheading'].value}</li>`
-    form.elements['newheading'].value="" //reset() will go to default value
-    console.log('heading changed')
-  }
+  const f = ev.target
+  const spellName = f.spellName.value
 
-form.addEventListener("submit", formChangeHeading)
- 
+  const spellsDiv = document.querySelector('#spells')
+  spellsDiv.innerHTML += `<li>${spellName}</li>`
+
+  f.reset()
+}
+
+form.addEventListener('submit', changeHeading)
