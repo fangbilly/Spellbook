32a033216b17d2dd8a946c79ddf0e57f3af773e5
fangz2 <fangz2@rose-hulman.edu>
2018-06-05 14:24:51 -0400

    two span in a li done by functions

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const changeHeading = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
-  const spellName = f.spellName.value
-
+  const spellName = document.createTextNode(f.spellName.value)
+  const gesture = document.createTextNode(f.gesture.value)
   const spellsDiv = document.querySelector('#spells')
-  spellsDiv.innerHTML += `<li>${spellName}</li>`
+  let a = buildListFromSpan(spellName,gesture)
+
+   spellsDiv.appendChild(a)
 
   f.reset()
+}
+
+function buildListFromSpan(spellName,gesture){
+  const spanspell = document.createElement("span")
+  const spangesture = document.createElement("span")
+  spanspell.setAttribute("class", "SpellName")
+  spangesture.setAttribute("class", "Gesture")
+  spanspell.appendChild(spellName)
+  spangesture.appendChild(gesture)  
+  return buildList(spanspell,spangesture)
+}
+
+function buildList(spanspell,spangesture){
+  const listitem = document.createElement("li")
+  listitem.appendChild(spanspell)
+  listitem.appendChild(spangesture)
+  return listitem
 }
 
 form.addEventListener('submit', changeHeading)
